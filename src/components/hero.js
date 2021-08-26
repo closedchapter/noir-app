dd6c891acc566ce4d21ada856b258f4e492df86a
horridharry <79926700+horridharry@users.noreply.github.com>
2021-08-26 19:21:34 +0100

    update hero.js
    Changed button

--- src/components/hero.js
+++ src/components/hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
                         <div className='text-base font-face-wm'>{data.hero.subheading}</div>
                         <div className='text-4xl font-face-wb'>{data.hero.headline}</div>
                     </div>
-                    <Link to='/discover' className='flex w-max mx-auto lg:mx-0 rounded font-face-wb text-base bg-black p-3 px-8 mt-12  text-white active:bg-pink-700'>Discover</Link>
+                    <Link to='/discover' className='flex w-max mx-auto lg:mx-0 rounded font-face-wb text-base bg-black p-3 px-8 mt-12  text-white active:bg-pink-700'>Enter</Link>
                 </div>
                 <div className='relative z-0 better-shadow overflow-hidden flex-1 rounded'>
                     <img className='h-full w-full object-cover' src={require('./images/cover-girl.jpeg').default} alt='' />
